aaf89f9c87e4ee415b92f76f874a6558fa2822ef
anandmnair <anand.manissery@gmail.com>
2017-02-08 22:40:55 +0530

    Update JavaTest.java

--- src/test/java/com/anand/demo/JavaTest.java
+++ src/test/java/com/anand/demo/JavaTest.java
@@ -1,8 +1,7 @@
 package com.anand.demo;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
-
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,7 +12,7 @@ import org.junit.Test;
 
 public class JavaTest {
 
-	List<Integer> integers;
+	private List<Integer> integers;
 	
 	@Before
 	public void setup() {
@@ -23,48 +22,48 @@ public class JavaTest {
 	@Test
 	public void createIntListFromOneToTen() {
 		List<Integer> integers = createIntegerList(1, 10);
-		assertThat(10, is(integers.size()));
+		assertThat(10, equalTo(integers.size()));
 	}
 	
 	@Test
 	public void findMaxFromList() {
 		Integer max = integers.stream().map(e->e).max(Integer::compareTo).get();
-		assertThat(5, is(max));
+		assertThat(5, equalTo(max));
 	}
 	
 	@Test
 	public void findMinFromList() {
 		Integer min = integers.stream().map(e->e).min(Integer::compareTo).get();
-		assertThat(1, is(min));
+		assertThat(1, equalTo(min));
 	}
 	
 	@Test
 	public void sortListAsc() {
 		integers.sort((o1,o2)->o1.compareTo(o2));
-		assertThat(1, is(integers.get(0)));
-		assertThat(5, is(integers.get(integers.size()-1)));
+		assertThat(1, equalTo(integers.get(0)));
+		assertThat(5, equalTo(integers.get(integers.size()-1)));
 	}
 	
 	@Test
 	public void sortListDesc() {
 		integers.sort((o1,o2)->o2.compareTo(o1));
-		assertThat(5, is(integers.get(0)));
-		assertThat(1, is(integers.get(integers.size()-1)));
+		assertThat(5, equalTo(integers.get(0)));
+		assertThat(1, equalTo(integers.get(integers.size()-1)));
 	}
 	
 	@Test
 	public void filterAllNumberExceptOneFromList() {
 		List<Integer> filteredList = integers.stream().filter(e-> e.equals(3)).collect(Collectors.toList());
-		assertThat(1, is(filteredList.size()));
-		assertThat(3, is(filteredList.get(0)));
+		assertThat(1, equalTo(filteredList.size()));
+		assertThat(3, equalTo(filteredList.get(0)));
 	}
 	
 	@Test
 	public void filterOldNumbersAndCollectOnlyEventNumbersFromList() {
 		List<Integer> filteredList = integers.stream().filter(e-> e%2==0).collect(Collectors.toList());
-		assertThat(2, is(filteredList.size()));
-		assertThat(2, is(filteredList.get(0)));
-		assertThat(4, is(filteredList.get(1)));
+		assertThat(2, equalTo(filteredList.size()));
+		assertThat(2, equalTo(filteredList.get(0)));
+		assertThat(4, equalTo(filteredList.get(1)));
 	}
 	
 	private List<Integer> createIntegerList(int from, int to) {
